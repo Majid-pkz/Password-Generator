cb425208a1c6470817d22766fe9887ad7c5ab470
Majid-pkz <majid.computing@gmail.com>
2023-04-24 16:43:26 +1000

    fix the bug with pass generator

--- script.js
+++ script.js
@@ -1,100 +1,80 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passChars="";
-var tempPassChars="";
-includeSpecial=false;
-isUserselected=true;
-// var securePass="a6"
-var finalPassword=""
- var numberOfChar=0;
+var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  captureCharNum();
- if(isUserselected==true){
-  charType();
-  for (var i = 0; i < numberOfChar; i++) {
-    var randomNumber = Math.floor(Math.random() * passChars.length);
-     finalPassword += passChars.substring(randomNumber, randomNumber +1);
-     
-   }
- }
+function generatePassword() {
+  // Prompt for password length
+  var passwordLength = parseInt(
+    window.prompt("How many Character would you like ?"),
+    10
+  );
+  // Prompt for password length
+  // var passwordLength = prompt("Enter password length (must be between 8 and 128 characters):");
+
+  // Check if user cancelled prompt or entered invalid input
+  if (
+    !passwordLength ||
+    isNaN(passwordLength) ||
+    passwordLength < 8 ||
+    passwordLength > 128
+  ) {
+    alert("password length must be between 8 and 128.");
+    return "";
+  }
+
+  // Prompt for character types to include
+  var includeLowercase = confirm("Include lowercase letters?");
+  var includeUppercase = confirm("Include uppercase letters?");
+  var includeNumbers = confirm("Include numbers?");
+  var includeSpecialChars = confirm("Include special characters?");
+
+  // Check if user selected at least one character type
+  if (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeNumbers &&
+    !includeSpecialChars
+  ) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  // Generate a password that meets the selected criteria
+  var password = "";
+  var availableChars = "";
+  if (includeLowercase) {
+    availableChars += "abcdefghijklmnopqrstuvwxyz";
+    password += getRandomChar("abcdefghijklmnopqrstuvwxyz");
+  }
+  if (includeUppercase) {
+    availableChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    password += getRandomChar("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  }
+  if (includeNumbers) {
+    availableChars += "0123456789";
+    password += getRandomChar("0123456789");
+  }
+  if (includeSpecialChars) {
+    availableChars += "!@#$%^&*()_+-=[]{}\\|;:'\",./<>?";
+    password += getRandomChar("!@#$%^&*()_+-=[]{}\\|;:'\",./<>?");
+  }
+  for (var i = password.length; i < passwordLength; i++) {
+    password += getRandomChar(availableChars);
+  }
+
+  return password;
+}
+
+// Generate a random character from a given character set
+function getRandomChar(charSet) {
+  return charSet.charAt(Math.floor(Math.random() * charSet.length));
+}
+
+// Event listener for the "Generate Password" button
+
+generateBtn.addEventListener("click", function () {
+  passwordText.value = "";
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passChars=password;
-  passwordText.value = password;
-  reset();
-
-}
-
-var captureCharNum = function() {
-  // Ask user the length of password they want
-   numberOfChar= window.prompt("How many Character would you like ?");
-  if (!numberOfChar) {
-    isUserselected=false;
-    return;
-      }
-  
-  
-     if(isNaN(numberOfChar)){
-        window.alert("Enter a type of Number")
-        numberOfChar= window.prompt("How many Character would you like ?");
-       
-           
-           if(isNaN(numberOfChar)){
-            alert("Please pay attention: you should enter a numerical value")
-            writePassword();
-          }
-        }   
-           
-      if(numberOfChar<8 ||numberOfChar>129){
-       window.alert("the number must be greater than 8 and less than 130")
-       writePassword();
-     
-
-    }
-
-  
- 
-} 
-
-
-  var charType=function(){
-    passChars=window.confirm("include special character?")
-    if(!passChars){
-      passChars="abcdefghijklmnopqrstuvwxyz"
-    }
-    else{
-      passChars="abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-      includeSpecial=true;
-    }
-   tempPassChars= window.confirm("include uppercase ");
-    if(tempPassChars && includeSpecial){
-    passChars="ABCDEFGHIJKLMNOPQRSVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()"
-    }
-      else if (tempPassChars && !includeSpecial){
-        passChars="ABCDEFGHIJKLMNOPQRSVWXYZabcdefghijklmnopqrstuvwxyz"
-      }
-      tempPassChars= window.confirm("include Numeric value?");
-      if(tempPassChars){
-        passChars=passChars+"123456789"
-      }
-
-      }
-
-  
-  
-
-    
-var generatePassword = function(){
- return password= finalPassword;
- }
-var reset = function(){
- numberOfChar= 0;
- passChars="";
- finalPassword="";
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  if (password) {
+    passwordText.value = password;
+  }
+});
